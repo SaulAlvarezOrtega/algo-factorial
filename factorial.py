5b27195c4e1e3033e9f1b83c4c9812b2f25786fc
Saul_Alvarez <ualvarez24622@gmail.com>
2024-02-01 13:39:09 -0700

    factorial

--- factorial.py
+++ factorial.py
@@ -1,3 +1,14 @@
 def factorial(num):
-	# your code here
-	pass
+	#for loop, num decrements and multiplys by a new variable (answer)
+	answer = 1
+	
+	if num == 0:
+		return 1
+	
+	
+	for x in range (num, 0, -1):
+		answer = answer * x
+		
+	return answer
+
+print(factorial(8))
